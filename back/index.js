2824bfe83351e4b6d9ac848ae970a247b6cca67d
Julien ORIOL <julien.oriol@etu.univ-cotedazur.fr>
2024-02-12 16:27:25 +0100

    feat: LA DB MARCHE (lien entre le conteneur de la db et le conteneur du back) Ref #11

--- back/index.js
+++ back/index.js
@@ -29,3 +29,22 @@ http.createServer(function (request, response) {
     }
 // For the server to be listening to request, it needs a port, which is set thanks to the listen function.
 }).listen(8000);
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb://mongo:27017/myapp_db";    // Lors de la connexion du back à mongo, cette db n'existe pas encore mais elle sera créée lors de l'insertion du premier élément
+const client = new MongoClient(uri);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connecté à MongoDB");
+        // Opérations sur la base de données
+    } catch (err) {
+        // Gère les erreurs de connexion
+        console.error("Erreur lors de la connexion à MongoDB:", err);
+    } finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
