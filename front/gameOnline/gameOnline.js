7c40b1b8655fd591adad95312ea6c29b7164a001
victor dedieu <louis.victordedieu@gmail.com>
2024-03-10 16:36:10 +0100

    suppression du bouton inutile #33

--- front/gameOnline/gameOnline.js
+++ front/gameOnline/gameOnline.js
@@ -3,8 +3,3 @@ import socket from "../sockets/socketConnection.js";
 window.onload = function() {
     socket.emit('findMatch', localStorage.getItem('token'));
 }
-
-const button = document.getElementById('newGameButton');
-button.addEventListener('click', function() {
-    socket.emit('test', 'test');
-});
