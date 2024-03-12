2df149e82170affc52d9052e11829226b5da5119
lucieandres <lucie.andres@etu.unice.fr>
2024-03-12 13:28:21 +0100

    you can find a friend by his username #36

--- front/home.js
+++ front/home.js
@@ -15,3 +15,37 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var friendsModal = document.getElementById("friendsModal");
+    var openFriendsPage = document.getElementById("openFriendsPage");
+    var friendsFrame = document.getElementById("friendsFrame");
+
+    openFriendsPage.addEventListener("click", function(e) {
+        e.preventDefault();
+        friendsFrame.src = "friendsPage/friends.html";
+        friendsModal.style.display = "flex";
+    });
+
+    window.addEventListener("click", function(event) {
+        if (event.target === friendsModal) {
+            friendsModal.style.display = "none";
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var accountModal = document.getElementById("accountModal");
+    var accountFrame = document.getElementById("accountFrame");
+
+    if (window.location.search.includes("login=true")) {
+        accountFrame.src = "accountPage/account.html";
+        accountModal.style.display = "flex";
+    }
+
+    window.addEventListener("click", function(event) {
+        if (event.target === accountModal) {
+            accountModal.style.display = "none";
+        }
+    });
+});
