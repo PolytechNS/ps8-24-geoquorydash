2fb92e91557cae9532bdf3068b6e5325a57f7d5a
Julien ORIOL <julien.oriol@etu.univ-cotedazur.fr>
2024-03-18 19:50:43 +0100

    feat: ajout d'une transition pour l'affichage du menu burger Ref #42

--- front/burgerMenu/burgerMenu.js
+++ front/burgerMenu/burgerMenu.js
@@ -1,5 +1,6 @@
 const burgerMenuButton = document.getElementById('burger-menu-button');
 const burgerMenuContainer = document.getElementById('burger-menu-container');
+const burgerMenuImage = burgerMenuButton.querySelector('img');
 let burgerMenuLoaded = false;
 
 burgerMenuButton.addEventListener('click', async () => {
@@ -7,10 +8,23 @@ burgerMenuButton.addEventListener('click', async () => {
         await loadBurgerMenu();
         burgerMenuLoaded = true;
     }
-    const burgerMenu = document.querySelector('.burger-menu');
-    burgerMenu.classList.toggle('show');
+    setTimeout(() => {
+        const burgerMenu = document.querySelector('.burger-menu');
+        burgerMenu.classList.toggle('show');
 
-    burgerMenuButton.classList.toggle('open', burgerMenu.classList.contains('show'));
+        if (burgerMenu.classList.contains('show')) {
+            burgerMenuImage.classList.add('rotate-image');
+            burgerMenuImage.classList.remove('rotate-image-reverse');
+            burgerMenuButton.style.left = 'calc(17vw + 20px)'; // Transition appliquée lors du changement
+        } else {
+            burgerMenuImage.classList.add('rotate-image-reverse');
+            burgerMenuImage.classList.remove('rotate-image');
+            burgerMenuButton.style.left = '20px'; // Transition appliquée lors du changement
+        }
+        
+
+        burgerMenuButton.classList.toggle('open', burgerMenu.classList.contains('show'));
+    }, 0);
 });
 
 async function loadBurgerMenu() {
