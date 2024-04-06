98f17447fbf85c2b4a09d2327443c9aed6a0c7a0
lucieandres <lucie.andres@etu.unice.fr>
2024-04-06 14:32:15 +0200

    arrangement de profil #58

--- front/accountPage/account.js
+++ front/accountPage/account.js
@@ -1,16 +1,12 @@
 const token = localStorage.getItem('token');
 
 if (token) {
-    console.log('Token:', token);
-    document.getElementsByClassName('sentence')[0].style.display = 'block';
-    document.getElementsByClassName('sentence')[1].style.display = 'block';
     document.getElementById('signup-btn').style.display = 'none';
     document.getElementById('login-btn').style.display = 'none';
 
     const deconnexionButton = document.getElementById('logout-btn');
     deconnexionButton.style.display = 'block';
-
-    deconnexionButton.addEventListener('click', function(event) {
+    deconnexionButton.addEventListener('click', function (event) {
         event.preventDefault();
 
         if (confirm('Êtes-vous sûr de vouloir vous déconnecter?')) {
